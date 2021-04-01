7bba14b47f4a9662feb3ae11ee8d0a6b2c2d8b29
webmanufactory <bubunya@yandex.ru>
2021-04-01 21:03:09 +0300

    Новый блок

--- main.js
+++ main.js
@@ -139,4 +139,15 @@ $(document).ready(function(){
                 serviceItemTwo.show();
             }
         });
+
+        $('.tab-trigger').click(function() {
+            var id = $(this).attr('data-tab'),
+                content = $('.tab-content[data-tab="'+ id +'"]');
+            
+            $('.tab-trigger.active').removeClass('active');
+            $(this).addClass('active');
+            
+            $('.tab-content.active').removeClass('active');
+            content.addClass('active');
+         });
   });
